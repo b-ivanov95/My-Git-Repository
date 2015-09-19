2aab209be4fe118d2d22abd314171cb164c4461b
b-ivanov95 <b_ivanov95@abv.bg>
2015-09-19 16:06:47 +0300

    Added two of the ArraysHomework Tasks

---
+++ Homeworks/ArraysHomework/ArraysHomework/01.SortArray/SortArray.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.SortArray
+{
+    class SortArray
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+             string sorted = SortMyArray(input);
+             Console.WriteLine(sorted);
+
+        }
+
+        private static string SortMyArray(string[] input)
+        {
+           int [] intArray = new int [input.Length];
+            for (int i = 0; i < input.Length; i++)
+			{
+                intArray[i] = int.Parse(input[i]);
+			}
+            Array.Sort(intArray);
+
+            string output = string.Join(" ", intArray);
+            return output;
+        }
+    }
+}
---
+++ Homeworks/ArraysHomework/ArraysHomework/02.SelectionSort/SelectionSort.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.SelectionSort
+{
+    class SelectionSort
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+            string sorted = SortSelection(input);
+            Console.WriteLine(sorted);
+        }
+
+        private static string SortSelection(string[] input)
+        {
+            int[] intArray = new int[input.Length];
+            for (int i = 0; i < input.Length; i++)
+            {
+                intArray[i] = int.Parse(input[i]);
+            }
+
+            for (int i = 0; i < intArray.Length-1; i++)
+            {
+                
+                for (int j = i+1; j < intArray.Length; j++)
+                {
+                   
+                    if (intArray[i] > intArray[j])
+                    {
+                        int temp = intArray[i];
+                        intArray[i] = intArray[j];
+                        intArray[j] = temp;
+
+                    }
+                }
+            }
+
+            string output = string.Join(" ",intArray);
+            return output;
+        }
+    }
+}
